1ec411fe4fba2a403750d1d153cace5d42e8980e
jhinier <jhiniersanchez@gmail.com>
2026-03-31 00:38:06 -0500

    sctuslización datos

---
+++ app/Http/Controllers/ControllerAdmin/EmprendimientoController.php
@@ -0,0 +1,7 @@
+public function index() {
+    return view('admin.emprendimientos.index'); // Solo entrega la vista
+}
+
+public function create() {
+    return view('admin.emprendimientos.create'); // Solo entrega el formulario
+}
